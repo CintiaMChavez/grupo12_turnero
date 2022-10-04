e0b5de1edece86b578673ca2c179ba8e4231b676
bflavia272 <bflavia272@gmail.com>
2022-10-03 23:52:13 -0300

    Create config.js

---
+++ backend/src/database/config/config.js
@@ -0,0 +1,10 @@
+require('dotenv').config()
+
+module.exports = {
+  "development": {
+    "username":"root",
+  
+    "database": "turnos_db",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
