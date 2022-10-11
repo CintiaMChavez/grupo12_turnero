b3ae412c373900c59d9d124deb4c71618bb9f29e
bflavia272 <bflavia272@gmail.com>
2022-10-11 00:15:31 -0300

    Create Footer.js

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( 
+        <h3 className="Footer">Desarrollado por Cintia y Luciana 
+        <span role = "img" aria-label = "emojis"> ✨✨ </span>
+      </h3>
+     );
+}
+ 
+export default Footer;
